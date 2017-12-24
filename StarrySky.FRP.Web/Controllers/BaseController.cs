32d622e5996a6df16537825e295aa0f1aa8e7b5e
starrysky-pfw <1130281533@qq.com>
2017-12-24 14:58:07 +0800

    Controller 全局异常捕获

--- StarrySky.FRP.Web/Controllers/BaseController.cs
+++ StarrySky.FRP.Web/Controllers/BaseController.cs
@@ -28,5 +28,17 @@ namespace StarrySky.FRP.Web.Controllers
             return Json(new Models.ResponseRst { Code = Models.RstCode.faild, Msg = msg });
         }
 
+        /// <summary>
+        /// 全局捕获 未捕获的异常   
+        /// </summary>
+        /// <param name="filterContext"></param>
+        protected override void OnException(ExceptionContext filterContext)
+        {
+            // 错误日志   后续要实现记录功能
+            string controllerNamer = filterContext.RouteData.Values["controller"].ToString();
+            string actionName = filterContext.RouteData.Values["action"].ToString();
+            string error = filterContext.Exception.Message.ToString();
+            base.OnException(filterContext);
+        }
     }
 }
